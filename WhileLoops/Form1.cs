1b3c2dd3eab18f3d38f0a5413be84e672d3b763d
Mr. Theodoropoulos <tony.theodoropoulos@ed.amdsb.ca>
2016-11-18 14:41:40 -0500

    Cleaned up variable names and reset loops to starting states

--- WhileLoops/Form1.cs
+++ WhileLoops/Form1.cs
@@ -47,20 +47,20 @@ namespace WhileLoops
             SolidBrush drawBrush = new SolidBrush(Color.White);
     
             //initialize
-            int x = this.Width-10;
+            int x = 0;
             int y = 150; 		    
             
             //test
-            while (x > 1)	
+            while (x < this.Width-20)	
             {
                 //act
                 g.Clear(Color.Black);
                 g.FillRectangle(drawBrush, x, y, 10, 10);
 
-                Thread.Sleep(10);
+                Thread.Sleep(5);
 
                 //change
-                x = x - 1;	
+                x++;
             }
         }
 
@@ -70,19 +70,19 @@ namespace WhileLoops
             SolidBrush drawBrush = new SolidBrush(Color.White);
 
             //initialize
-            int x = 1;
+            int pixels = 1;
           	    
             //test
-            while (x < 50)	
+            while (pixels < 50)	
             {
                 //act
                 g.Clear(Color.Black);
-                g.FillRectangle(drawBrush, 50 - x / 2, 150 - x / 2, 10 + x, 10 + x);
-
-                Thread.Sleep(10);
+                g.FillRectangle(drawBrush, 50, 150, 50 + pixels, 50 + pixels);
+            
+                Thread.Sleep(5);
 
                 //change
-                x = x + 1;	
+                pixels++;	
             }
         }
 
@@ -92,20 +92,21 @@ namespace WhileLoops
             SolidBrush drawBrush = new SolidBrush(Color.White);
 
             //initialize
-            int x = 1;
+            int rgbVal = 1;
 
             //test
-            while (x < 256)
+            while (rgbVal < 256)
             {
                 //act
+                drawBrush.Color = Color.FromArgb(0 + rgbVal, 0 , 0);
+
                 g.Clear(Color.Black);
-                drawBrush.Color = Color.FromArgb(255, 255 - x , 0 + x);
                 g.FillRectangle(drawBrush, 50, 150, 50, 50);
 
                 Thread.Sleep(10);
 
                 //change
-                x = x + 1;
+                rgbVal++;
             }
         }
     }
